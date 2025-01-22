a4c9a93ea1c26c2f579a39dd8ae970f595b7f39d
IsaFernandes22 <isabellafernandesde@mines.edu>
2025-01-22 12:53:58 -0700

    began callibration script

--- src/cameraCallibration.py
+++ src/cameraCallibration.py
@@ -1,3 +1,15 @@
 # @Author: Isabella Fernandes
 # This callibrates the camera used in the prototype. 
 
+import cv2
+import os
+import numpy as np
+
+# Arrays to store object points and image points from all the given images.
+objPoints3D = []
+imgPoints2D = []
+
+# termination criteria
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001) # TODO
+
+
