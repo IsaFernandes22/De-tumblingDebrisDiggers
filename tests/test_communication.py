96cf03fc5f1a4495134c6d47d3320573c732faca
IsaFernandes22 <isabellafernandesde@mines.edu>
2025-02-10 14:47:23 -0700

    added some tests

--- tests/test_communication.py
+++ tests/test_communication.py
@@ -11,4 +11,21 @@ class TestCommunication(unittest.TestCase):
     def setUp(self):
         return super().setUp()
     
+    #TODO need to make these test more robust
+
+    # Test logging message
+    def test_send_log(self):
+        message = "Test log message"
+        sock = CommunicationHandler.send_log(message)
+        self.assertTrue(sock)
     
+    # Test listening to arduino
+    def test_listen_to_arduino(self):
+        arduino = CommunicationHandler.listen_to_arduino()
+        self.assertTrue(arduino)
+    
+    # Test notifying ground station
+    def test_notify_ground_station(self):
+        message = "Test message"
+        sock = CommunicationHandler.notify_ground_station(message)
+        self.assertTrue(sock)
