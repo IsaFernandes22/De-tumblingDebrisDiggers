e8d1d5ff2dab48f86cdfb90ab62dd14a81a28965
IsaFernandes22 <isabellafernandesde@mines.edu>
2025-02-27 10:03:52 -0700

    camera has been calibrated (with a computer, will need to be recalibrated for the final prototype) '

--- tests/test_dispatch.py
+++ tests/test_dispatch.py
@@ -1,42 +1,50 @@
 #this is for trial testing OpenCV and Supervision for launch params
-
 import unittest
 from src import comms
 from src import dispatch
-from src import main
+from unittest.mock import patch
 
 class TestDispatch(unittest.TestCase):
 
     def setUp(self):
-        return super().setUp()
+        self.cap = comms.init_camera()
     
-    # Test if the camera is initialized correctly
+    # Test if the camera initializes correctly
     def test_init_camera(self):
-        cap = comms.init_camera()
-        self.assertTrue(cap.isOpened())
-        cap.release()
-    
-     # Test if the frame is processed correctly (location and velocity are returned)
+        self.assertTrue(self.cap.isOpened())
+
+    # Test if the frame is processed correctly (location and velocity are returned)
     def test_processFrame(self):
-        cap = comms.init_camera()
-        ret, frame = cap.read()
+        ret, frame = self.cap.read()
         previous_location = None
         dt = 1
         velocity, location = comms.processFrame(frame, previous_location, dt)
         self.assertIsInstance(velocity, tuple)
         self.assertIsInstance(location, tuple)
-        cap.release()
 
-    # Test if the calculated location and velocity are correct
+    # Test if location and velocity are reasonable values
     def test_processFrame_values(self):
-        cap = comms.init_camera()
-        ret, frame = cap.read()
+        ret, frame = self.cap.read()
         previous_location = None
         dt = 1
         velocity, location = comms.processFrame(frame, previous_location, dt)
-        self.assertGreaterEqual(location[0], 0) # location should be positive
-        self.assertGreaterEqual(location[1], 0) # TODO change these values to the actual values
-        cap.release()
-    
+
+        self.assertGreaterEqual(location[0], 0)  # Location X should be positive
+        self.assertGreaterEqual(location[1], 0)  # Location Y should be positive
+
+    # Test if `dispatch` properly handles the processed frame data
+    @patch("src.dispatch.send_data")
+    def test_dispatch_receives_correct_data(self, mock_send_data):
+        ret, frame = self.cap.read()
+        previous_location = None
+        dt = 1
+        velocity, location = comms.processFrame(frame, previous_location, dt)
+        
+        # Call the function that sends data to dispatch
+        dispatch.send_data({"velocity": velocity, "location": location})
+
+        # Check if dispatch.send_data was called with correct values
+        mock_send_data.assert_called_with({"velocity": velocity, "location": location})
+
     def tearDown(self):
-        return super().tearDown()
+        self.cap.release()
