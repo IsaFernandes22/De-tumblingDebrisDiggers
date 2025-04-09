b1b0408d30c548e3656216cb53197c5a1b4b4bd7
QHej <160056317+QHej@users.noreply.github.com>
2025-04-09 17:15:13 -0600

    Update AttachmentCodeOnly.ino

--- src/Control Code/AttachmentCodeOnly.ino
+++ src/Control Code/AttachmentCodeOnly.ino
@@ -1,70 +1,35 @@
-#include <TMCStepper.h>         // TMCstepper - https://github.com/teemuatlut/TMCStepper
-#include <SoftwareSerial.h>     // Software serial for the UART to TMC2209 - https://www.arduino.cc/en/Reference/softwareSerial
-#include <Streaming.h>          // For serial debugging output - https://www.arduino.cc/reference/en/libraries/streaming/
+const int limitSwitchPins[3] = {0,0,0}; // order: spinning, horizontal, vertical
 
-int limitSwitchPins[3] = [0,0,0]; // order: spinning, horizontal, vertical
+const int horizontalMotorPins[3] = {2,3,4}; //enable, dir, step. Any pins on the board work
+const int verticalMotorPins[3] = {0,0,0};
+const int spinMotorPins[3] = {0,0,0};
+const int drillMotorPins[2] = {0,0}; // dir, volt. volt must be PWM enabled
 
-int horizontalMotorPins[6] = {2,3,4,5,6,7}; //enable, dir, step, clk, tx, rx
-int verticalMotorPins[6] = {0,0,0,0,0,0};
-int spinMotorPins[6] = {0,0,0,0,0,0};
-int drillMotorPins[3] = {0,0,0}
+const int stepperSpeed = 160; // inverted. A smaller number means faster
 
-SoftwareSerial horizontalSerial(horizontalMotorPins[5], horizontalMotorPins[4]);
-SoftwareSerial verticalSerial(verticalMotorPins[5], verticalMotorPins[4]);
-SoftwareSerial spinSerial(spinMotorPins[5], spinMotorPins[4]);
-TMC2209Stepper horizontalMotor(&horizontalSerial, 0.11f, 0b00);
-TMC2209Stepper verticalMotor(&verticalSerial, 0.11f, 0b00);
-TMC2209Stepper spinMotor(&spinSerial, 0.11f, 0b00);
+// likely need a seperate speed variable for the vertical movement, 
+// so that we can adjust the speed that the drill goes up and down,
+// but ¯\_(ツ)_/¯. Syncing that will be a task for later
+
 
 void drill();
 void reset();
 
 void setup() {
 
-  pinMode(limitSwitchPins[0], INPUT);
-  pinMode(limitSwitchPins[1], INPUT);
-  pinMode(limitSwitchPins[2], INPUT);
+  for(int i = 0; i < 3; i++){
+    pinMode(limitSwitchPins[i], INPUT);
+    pinMode(horizontalMotorPins[i], OUTPUT);
+    pinMode(verticalMotorPins[i], OUTPUT);
+    pinMode(spinMotorPins[i], OUTPUT);
+  }
 
+  pinMode(drillMotorPins[0], OUTPUT);
+  pinMode(drillMotorPins[1], OUTPUT);
 
-  horizontalSerial.begin(11520);           // initialize software serial for UART motor control
-  verticalSerial.begin(11520);
-  spinSerial.begin(11520);
-  horizontalMotor.beginSerial(11520);      // Initialize UART
-  verticalMotor.beginSerial(11520);
-  spinMotor.beginSerial(11520);
-
-  pinMode(horizontalMotorPins[0], OUTPUT);
-  pinMode(horizontalMotorPins[1], OUTPUT);
-  pinMode(horizontalMotorPins[2], OUTPUT);
-  pinMode(verticalMotorPins[0], OUTPUT);
-  pinMode(verticalMotorPins[1], OUTPUT);
-  pinMode(verticalMotorPins[2], OUTPUT);
-  pinMode(spinMotorPins[0], OUTPUT);
-  pinMode(spinMotorPins[1], OUTPUT);
-  pinMode(spinMotorPins[2], OUTPUT);
-  digitalWrite(horizontalMotorPins[0], LOW); // enable!
+  digitalWrite(horizontalMotorPins[0], LOW); // this enables all steppers
   digitalWrite(verticalMotorPins[0], LOW);
-  digitalWrite(spinMotorPins[0], LOW);
-
-  // no idea what most of this next block is doing. Investigate later?
-  horizontalMotor.begin();
-  horizontalMotor.toff(5); 
-  horizontalMotor.rms_current(500); 
-  horizontalMotor.microsteps(256); 
-  horizontalMotor.en_spreadCycle(false);
-  horizontalMotor.pwm_autoscale(true);
-  verticalMotor.begin();
-  verticalMotor.toff(5); 
-  verticalMotor.rms_current(500); 
-  verticalMotor.microsteps(256); 
-  verticalMotor.en_spreadCycle(false);
-  verticalMotor.pwm_autoscale(true); 
-  spinMotor.begin();
-  spinMotor.toff(5); 
-  spinMotor.rms_current(500); 
-  spinMotor.microsteps(256); 
-  spinMotor.en_spreadCycle(false);
-  spinMotor.pwm_autoscale(true);
+  digitalWrite(verticalMotorPins[0], LOW);
 
 }
 
@@ -72,40 +37,57 @@ void loop() {
   
   reset(); // positions the system such that all limit switches are triggered
 
-  //move to central drill position
-  horizontalMotor.VACTUAL(300000);
-  horizontalMotor.shaft(1); // direction; may need to be switched depending on which side the limit switch is on
-  horizontalSerial << horizontalMotor.VACTUAL() << endl;
-  delay(300); // tune amount of time based on how long it takes to get to the correct position (yes, this is the reccomended way to do this. I know it's janky)
-  horizontalMotor.VACTUAL(0);
-  horizontalSerial << horizontalMotor.VACTUAL() << endl;
+  // Move to central drill position:
+
+  int time1 = 10000; // tune this value! based on how long it takes to move drill to correct spot
+  
+  digitalWrite(horizontalMotorPins[1], LOW); // idk man I just guessed a direction
+  
+  for (int t = 0; t < time1; t++){
+    digitalWrite(horizontalMotorPins[2], HIGH);
+    delayMicroseconds(stepperSpeed);
+    digitalWrite(horizontalMotorPins[2], LOW);
+    delayMicroseconds(stepperSpeed);
+  }
 
   drill(); //central hole
 
-  horizontalMotor.VACTUAL(300000);
-  horizontalMotor.shaft(1); // direction; may need to be switched depending on which side the limit switch is on
-  horizontalSerial << horizontalMotor.VACTUAL() << endl;
-  delay(100); // tune amount of time based on how long it takes to get to the correct position
-  horizontalMotor.VACTUAL(0);
-  horizontalSerial << horizontalMotor.VACTUAL() << endl;
+  int time2 = 10000; // tune this value! based on how long it takes to move drill to correct spot
+  
+  digitalWrite(horizontalMotorPins[1], HIGH); // idk man I just guessed a direction
+  
+  for (int t = 0; t < time1; t++){
+    digitalWrite(horizontalMotorPins[2], HIGH);
+    delayMicroseconds(stepperSpeed);
+    digitalWrite(horizontalMotorPins[2], LOW);
+    delayMicroseconds(stepperSpeed);
+  }
 
   drill(); // first outside hole
 
-  spinMotor.VACTUAL(300000);
-  spinMotor.shaft(1); // direction; may need to be switched depending on which side the limit switch is on
-  spinSerial << spinMotor.VACTUAL() << endl;
-  delay(100); // tune amount of time based on how long it takes to get to the correct position
-  spinMotor.VACTUAL(0);
-  spinSerial << spinMotor.VACTUAL() << endl;
+  int time3 = 10000; // tune this value! based on how long it takes to move drill to correct spot
+  
+  digitalWrite(spinMotorPins[1], LOW); // idk man I just guessed a direction
+  
+  for (int t = 0; t < time1; t++){
+    digitalWrite(spinMotorPins[2], HIGH);
+    delayMicroseconds(stepperSpeed);
+    digitalWrite(spinMotorPins[2], LOW);
+    delayMicroseconds(stepperSpeed);
+  }
   
   drill(); // second outside hole
 
-  spinMotor.VACTUAL(300000);
-  spinMotor.shaft(1); // direction; may need to be switched depending on which side the limit switch is on
-  spinSerial << spinMotor.VACTUAL() << endl;
-  delay(100); // tune amount of time based on how long it takes to get to the correct position
-  spinMotor.VACTUAL(0);
-  spinSerial << spinMotor.VACTUAL() << endl;
+  int time4 = 10000; // tune this value! based on how long it takes to move drill to correct spot
+  
+  digitalWrite(spinMotorPins[1], LOW); // idk man I just guessed a direction
+  
+  for (int t = 0; t < time1; t++){
+    digitalWrite(spinMotorPins[2], HIGH);
+    delayMicroseconds(stepperSpeed);
+    digitalWrite(spinMotorPins[2], LOW);
+    delayMicroseconds(stepperSpeed);
+  }
 
   drill(); // third outside hole
 
@@ -115,42 +97,62 @@ void loop() {
 
 void reset(){
   //run steppers to reset position, specically running until they hit the limit switches
-  while (digitalRead(limitSwitchPins[0]) == LOW){ // may have to switch specifics here, the idea is "while the limit switch isn't triggered"
-    spinMotor.VACTUAL(300000);
-    spinMotor.shaft(0); // direction; may need to be switched depending on which side the limit switch is on
-    spinSerial << spinMotor.VACTUAL() << endl;
-  }
-  spinMotor.VACTUAL(0);
-  spinSerial << spinMotor.VACTUAL() << endl;
 
-  while (digitalRead(limitSwitchPins[1]) == LOW){ // may have to switch specifics here, the idea is "while the limit switch isn't triggered"
-    horizontalMotor.VACTUAL(300000);
-    horizontalMotor.shaft(0); // direction; may need to be switched depending on which side the limit switch is on
-    horizontalSerial << horizontalMotor.VACTUAL() << endl;
-  }
-  horizontalMotor.VACTUAL(0);
-  horizontalSerial << horizontalMotor.VACTUAL() << endl;
+  digitalWrite(horizontalMotorPins[1], HIGH); // idk man I just guessed a direction
+  digitalWrite(verticalMotorPins[1], HIGH);
+  digitalWrite(spinMotorPins[1], HIGH);
 
-  while (digitalRead(limitSwitchPins[2]) == LOW){ // may have to switch specifics here, the idea is "while the limit switch isn't triggered"
-    verticalMotor.VACTUAL(300000);
-    verticalMotor.shaft(0); // direction; may need to be switched depending on which side the limit switch is on
-    verticalSerial << verticalMotor.VACTUAL() << endl;
+   while (digitalRead(limitSwitchPins[0]) == LOW) {
+    digitalWrite(spinMotorPins[2], HIGH);
+    delayMicroseconds(stepperSpeed);
+    digitalWrite(spinMotorPins[2], LOW);
+    delayMicroseconds(stepperSpeed);
   }
-  verticalMotor.VACTUAL(0);
-  verticalSerial << verticalMotor.VACTUAL() << endl;
+  
+  while (digitalRead(limitSwitchPins[1]) == LOW) {
+    digitalWrite(horizontalMotorPins[2], HIGH);
+    delayMicroseconds(stepperSpeed);
+    digitalWrite(horizontalMotorPins[2], LOW);
+    delayMicroseconds(stepperSpeed);
+  }
+
+  while (digitalRead(limitSwitchPins[2]) == LOW) {
+    digitalWrite(verticalMotorPins[2], HIGH);
+    delayMicroseconds(stepperSpeed);
+    digitalWrite(verticalMotorPins[2], LOW);
+    delayMicroseconds(stepperSpeed);
+  }
 }
 
 void drill(){
 
-  verticalMotor.VACTUAL(300000);
-  verticalMotor.shaft(1); // direction; may need to be switched depending on which side the limit switch is on
-  verticalSerial << verticalMotor.VACTUAL() << endl;
+  //start the DC motor spinning
+  digitalWrite(drillMotorPins[0], HIGH); // idk high or low
+  analogWrite(drillMotorPins[1], 200); // change the PWM to adjust the speed. Will have to sync this number with how fast it moves up and down???
 
-  //set DC motor PWM. Will have to figure out the correct PWM to keep DC and stepper synced
+  //move the drill "down"
+  digitalWrite(verticalMotorPins[1], HIGH);
+  int drillTime = 10000;
 
-  delay(100); // how long does the drilling take?
-  verticalMotor.VACTUAL(0);
-  spinSerial << spinMotor.VACTUAL() << endl;
+  for (int t = 0; t < drillTime; t++){
+    digitalWrite(verticalMotorPins[2], HIGH);
+    delayMicroseconds(stepperSpeed);
+    digitalWrite(verticalMotorPins[2], LOW);
+    delayMicroseconds(stepperSpeed);
+  }
+
+  //stop the drill motor
+  analogWrite(drillMotorPins[1], 0);
+
+  //move the drill "up"
+  digitalWrite(verticalMotorPins[1], LOW);
+
+  for (int t = 0; t < drillTime; t++){
+    digitalWrite(verticalMotorPins[2], HIGH);
+    delayMicroseconds(stepperSpeed);
+    digitalWrite(verticalMotorPins[2], LOW);
+    delayMicroseconds(stepperSpeed);
+  }
 
 }
 
