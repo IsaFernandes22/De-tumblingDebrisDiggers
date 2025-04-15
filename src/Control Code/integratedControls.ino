1686394885ed0de5c9668678c951c64cee48e005
IsaFernandes22 <isabellafernandesde@mines.edu>
2025-04-15 08:26:57 -0600

    organized controls a bit

--- src/Control Code/integratedControls.ino
+++ src/Control Code/integratedControls.ino
@@ -3,22 +3,27 @@
 
 #include <TMCStepper.h>         // TMCstepper - https://github.com/teemuatlut/TMCStepper
 #include <SoftwareSerial.h>     // Software serial for the UART to TMC2209 - https://www.arduino.cc/en/Reference/softwareSerial
-#include <Streaming.h>          // For serial debugging output - https://www.arduino.cc/reference/en/libraries/streaming/
+#include <Streaming.h>          // For serial debugging output - https://www.arduino.cc/reference/en/libraries/streaming/\
+
 
 // Define FSM states
 enum State {
-    PRE_DISPATCH,
-    DISPATCHED,
-    DISPATCH_STATE,
-    CONTACT,
-    ATTACHMENT,
-    FOURTH_DRILL_DONE,
-    DETUMBLE,
-    ROTATION_CHECK,
-    DONE
+  PRE_DISPATCH,
+  DISPATCHED,
+  DISPATCH_STATE,
+  CONTACT,
+  ATTACHMENT,
+  FOURTH_DRILL_DONE,
+  DETUMBLE,
+  ROTATION_CHECK,
+  DONE
 };
 // Initialize FSM state variable
 State currentState = PRE_DISPATCH;
+void drill();
+void reset();
+void moveCMGToPotentiometerAngle();
+void readGyro();
 
 // Function to transition between states
 void transitionTo(State newState) {
@@ -26,7 +31,7 @@ currentState = newState;
 Serial.print("Transitioned to state: ");
 Serial.println(newState);
 }
-
+//------------------Pin Assignments-----------------------------------------------------------------
 int limitSwitchPins[3] = {0, 0, 0}; // order: spinning, horizontal, vertical
 
 int horizontalMotorPins[6] = {2, 3, 4, 5, 6, 7}; // enable, dir, step, clk, tx, rx
@@ -49,9 +54,7 @@ int maxAngle = 180; // Maximum allowed angle
 int stepDelay = 500; // Delay between steps in microseconds
 int stepCount = 0; // Counter for steps
 
-void drill();
-void reset();
-
+//------------------Setup-----------------------------------------------------------------
 void setup() {
   pinMode(limitSwitchPins[0], INPUT);
   pinMode(limitSwitchPins[1], INPUT);
@@ -97,6 +100,88 @@ void setup() {
   spinMotor.pwm_autoscale(true);
 }
 
+//------------------Main Loop-----------------------------------------------------------------
+void loop() {
+  // FSM logic: Switch based on current state
+  // TODO: make this actually work with the states
+  switch (currentState) {
+    case PRE_DISPATCH:
+      Serial.println("In PRE_DISPATCH state.");
+      // Wait for dispatch signal (this is a placeholder, adapt to your actual condition)
+      // if (dispatchConditionMet) {
+      transitionTo(DISPATCHED);
+      break;
+
+    case DISPATCHED:
+      Serial.println("In DISPATCHED state.");
+      // Wait for dispatched state to complete
+      // if (dispatchedConditionMet) {
+      transitionTo(DISPATCH_STATE);
+      break;
+
+    case DISPATCH_STATE:
+      Serial.println("In DISPATCH_STATE.");
+      // Mite in transit to the RSO
+      // Add check to transition to CONTACT when the RSO is reached
+      // TODO: sampling the gyroscope to check if collision is made
+      transitionTo(CONTACT); // Assuming it reaches contact immediately (adjust as needed)
+      break;
+
+    case CONTACT:// change state to attachment
+      Serial.println("In CONTACT state.");
+      // Mite makes contact with the RSO
+      transitionTo(ATTACHMENT);
+      break;
+
+    case ATTACHMENT:
+      Serial.println("In ATTACHMENT state.");
+      // TODO: control the drill code
+      // Once attached, wait for the 4th drill to finish
+      // if (drillCount == 4) {
+      transitionTo(FOURTH_DRILL_DONE);
+      break;
+
+    case FOURTH_DRILL_DONE:
+      Serial.println("4th drill done, transitioning to DETUMBLE state.");
+      // Activate CMG and send log bit '1' to host satellite
+      // TODO: change states
+      activateCMG();
+      transitionTo(DETUMBLE);
+      break;
+
+    case DETUMBLE:
+      Serial.println("In DETUMBLE state.");
+      //TODO: activateCMG controls
+      // CMG is activated to stabilize the RSO
+      sendLogToHost("1"); // Log '1' bit to host satellite
+      // Check rotation status and transition to ROTATION_CHECK
+      if (checkRotationStatus()) {
+        transitionTo(ROTATION_CHECK);
+      }
+      break;
+
+    case ROTATION_CHECK:
+      Serial.println("In ROTATION_CHECK state.");
+      // Check if rotation is less than 2 degrees
+      if (rotationIsLessThan2Degrees()) {
+        transitionTo(DONE);
+      }
+      break;
+
+    case DONE:
+      Serial.println("In DONE state.");
+      // Final state, turn off CMG and send log '12345' to host satellite
+      deactivateCMG();
+      sendLogToHost("12345");
+      // Optionally reset or end the operation
+      transitionTo(PRE_DISPATCH);
+      break;
+  }
+
+  delay(1000); // Delay between state transitions
+}
+
+//------------------Reset-----------------------------------------------------------------
 void reset() {
   // Run steppers to reset position until they hit the limit switches
   while (digitalRead(limitSwitchPins[0]) == LOW) {
@@ -123,6 +208,8 @@ void reset() {
   verticalMotor.VACTUAL(0);
   verticalSerial << verticalMotor.VACTUAL() << endl;
 }
+
+//------------------Helper Functions-----------------------------------------------------------------
 
 void drill() {
   // Activate the vertical motor for drilling
@@ -168,81 +255,6 @@ void moveCMGToPotentiometerAngle() {
   Serial.println("CMG Positioning Complete");
 }
 
-void loop() {
-  // FSM logic: Switch based on current state
-  // TODO: make this actually work with the states
-  switch (currentState) {
-    case PRE_DISPATCH:
-      Serial.println("In PRE_DISPATCH state.");
-      // Wait for dispatch signal (this is a placeholder, adapt to your actual condition)
-      // if (dispatchConditionMet) {
-      transitionTo(DISPATCHED);
-      break;
-
-    case DISPATCHED:
-      Serial.println("In DISPATCHED state.");
-      // Wait for dispatched state to complete
-      // if (dispatchedConditionMet) {
-      transitionTo(DISPATCH_STATE);
-      break;
-
-    case DISPATCH_STATE:
-      Serial.println("In DISPATCH_STATE.");
-      // Mite in transit to the RSO
-      // Add check to transition to CONTACT when the RSO is reached
-      transitionTo(CONTACT); // Assuming it reaches contact immediately (adjust as needed)
-      break;
-
-    case CONTACT:
-      Serial.println("In CONTACT state.");
-      // Mite makes contact with the RSO
-      transitionTo(ATTACHMENT);
-      break;
-
-    case ATTACHMENT:
-      Serial.println("In ATTACHMENT state.");
-      // Once attached, wait for the 4th drill to finish
-      // if (drillCount == 4) {
-      transitionTo(FOURTH_DRILL_DONE);
-      break;
-
-    case FOURTH_DRILL_DONE:
-      Serial.println("4th drill done, transitioning to DETUMBLE state.");
-      // Activate CMG and send log bit '1' to host satellite
-      activateCMG();
-      transitionTo(DETUMBLE);
-      break;
-
-    case DETUMBLE:
-      Serial.println("In DETUMBLE state.");
-      // CMG is activated to stabilize the RSO
-      sendLogToHost("1"); // Log '1' bit to host satellite
-      // Check rotation status and transition to ROTATION_CHECK
-      if (checkRotationStatus()) {
-        transitionTo(ROTATION_CHECK);
-      }
-      break;
-
-    case ROTATION_CHECK:
-      Serial.println("In ROTATION_CHECK state.");
-      // Check if rotation is less than 2 degrees
-      if (rotationIsLessThan2Degrees()) {
-        transitionTo(DONE);
-      }
-      break;
-
-    case DONE:
-      Serial.println("In DONE state.");
-      // Final state, turn off CMG and send log '12345' to host satellite
-      deactivateCMG();
-      sendLogToHost("12345");
-      // Optionally reset or end the operation
-      transitionTo(PRE_DISPATCH);
-      break;
-  }
-
-  delay(1000); // Delay between state transitions
-}
 
 // Helper function to simulate checking rotation status
 bool checkRotationStatus() {
@@ -275,4 +287,44 @@ void sendLogToHost(const char* log) {
   // TODO Send the log to the host satellite (e.g., via serial)
   Serial.print("Sending log to host: ");
   Serial.println(log);
+}
+
+void readGyro(){
+
+  float declinationAngle = (11.0 - (41.0 / 60.0)) / (180 / PI); // may need to fix this number
+  compass.setDeclinationAngle(declinationAngle);
+
+  float xBuffer = 0;
+  float yBuffer = 0;
+  float zBuffer = 0;
+
+  float startTime = millis();
+
+  sVector_t mag = compass.readRaw();
+  compass.getHeadingDegrees();
+
+  float prevX = mag.XAxis();
+  float prevY = mag.YAxis();
+  float prevZ = mag.ZAxis();
+
+  
+  for (int sample = 0; sample < 100; sample++){ // consider tuning the number of samples
+    sVector_t mag = compass.readRaw();
+    compass.getHeadingDegrees();
+
+    xBuffer += mag.XAxis() - prevX;
+    yBuffer += mag.YAxis() - prevY;
+    zBuffer += mag.ZAxis() - prevZ;
+
+    prevX = mag.XAxis();
+    prevY = mag.YAxis();
+    prevZ = mag.ZAxis();
+    
+    //may need to toss a delay in here
+  }
+  
+  currentAngVel(xAxis) = xBuffer/(millis() - startTime); // FIX: none of these lines have the correct unit conversions yet 
+  currentAngVel(yAxis) = yBuffer/(millis() - startTime);
+  currentAngVel(zAxis) = zBuffer/(millis() - startTime);
+
 }
